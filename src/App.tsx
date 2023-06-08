41040059c8eef79879be72fb356bd5eba6161560
chris <cs.communication.dev@gmail.com>
2023-06-08 15:36:48 +0200

    add: hero and herostyle

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,18 @@ import { Navbar } from './components/Narbar/Navbar';
 
 function App() {
   return (
-    <div>
+    <div className="flex_container">
       <Navbar/>
+      <div className="content_container">
+        <div className="hero_container">
+          <p className="hero_title"> Default</p>
+          <p className="hero_text">This is a Dashboard example built using this template</p>
+          <div className="round-background">
+          <div className="plus-icon">+</div>
+        </div>
+        </div>
+      </div>
+
     </div>
   );
 }
