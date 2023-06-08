782b959e41555b6616d3bb37ffb2b282bb7f61e1
guillaume.mucwicki@digi-service.fr <guillaume.mucwicki@gmail.com>
2023-06-08 15:09:50 +0200

    feat: Add Footer component with links and copyright notice
    - Add a footer component to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './components/Narbar/Navbar';
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
