2b5e0a920a22dd6c99cb07541609e3050d750847
guillaume.mucwicki@digi-service.fr <guillaume.mucwicki@gmail.com>
2023-06-09 10:13:04 +0200

    🧹

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from './components/narbar/Navbar';
-import Footer from "./components/footer/Footer";
+import Footer from "./components/footer/footer";
 import CardsContainer from "./components/card/card";
+import LogsCard from "./components/log_card/log_card";
 
 function App() {
     return (
@@ -10,15 +11,16 @@ function App() {
             <Navbar/>
             <div className="content_container">
                 <div className="c_pas_propre_container">
-                <div className="hero_container">
-                    <p className="hero_title"> Default</p>
-                    <p className="hero_text">This is a Dashboard example built using this template</p>
-                    <div className="round-background">
-                        <div className="plus-icon">+</div>
+                    <div className="hero_container">
+                        <p className="hero_title"> Default</p>
+                        <p className="hero_text">This is a Dashboard example built using this template</p>
+                        <div className="round-background">
+                            <div className="plus-icon">+</div>
+                        </div>
                     </div>
-                </div>
-                <CardsContainer/>
-
+                    <CardsContainer/>
+                    <br/>
+                    <LogsCard/>
                 </div>
             </div>
             <Footer/>
