3931ee9a201c2e60ee9bbd56733b065efcaeafdf
guillaume.mucwicki@digi-service.fr <guillaume.mucwicki@gmail.com>
2023-06-09 08:20:44 +0200

    🥸

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import './App.css';
-import { Navbar } from './components/narbar/Navbar';
+import {Navbar} from './components/narbar/Navbar';
 import Footer from "./components/footer/Footer";
 import CardsContainer from "./components/card/card";
 
 function App() {
-  return (
-    <div>
-      <Navbar/>
-      <div className="content_container">
-        <div className="hero_container">
-          <p className="hero_title"> Default</p>
-          <p className="hero_text">This is a Dashboard example built using this template</p>
-          <div className="round-background">
-          <div className="plus-icon">+</div>
-        </div>
-        </div>
-      </div>
+    return (
+        <div>
+            <Navbar/>
+            <div className="content_container">
+                <div className="c_pas_propre_container">
+                <div className="hero_container">
+                    <p className="hero_title"> Default</p>
+                    <p className="hero_text">This is a Dashboard example built using this template</p>
+                    <div className="round-background">
+                        <div className="plus-icon">+</div>
+                    </div>
+                </div>
+                <CardsContainer/>
 
-      <CardsContainer/>
-      <Footer/>
-    </div>
-  );
+                </div>
+            </div>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
